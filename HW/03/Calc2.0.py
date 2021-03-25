cfbbece587f9412e5cbb8e2aff9cb147304f50c9
Lissa-exe <80700705+Lissa-exe@users.noreply.github.com>
2021-03-25 11:41:45 +0200

    Update Calc2.0.py

--- HW/03/Calc2.0.py
+++ HW/03/Calc2.0.py
@@ -1,21 +1,12 @@
-import sys
-
 first_value: str = input('Input first value: ')
 operation: str = input('Input math operation: ')
 second_value: str = input('Input second value: ')
 
 try:
-    first_value = int(first_value)
-    second_value = int(second_value)
+    first_value: int = int(first_value)
+    second_value: int = int(second_value)
 except ValueError:
     print('Not a digit')
-    sys.exit(1)
-
-try:
-    first_value / second_value or first_value // second_value
-except ZeroDivisionError:
-    print('Error: Zero division')
-    sys.exit(1)
 
 if operation == '+':
     print(first_value + second_value)
@@ -25,6 +16,16 @@ elif operation == '*':
     print(first_value * second_value)
 elif operation == '**':
     print(first_value ** second_value)
+elif operation == '/':
+    try:
+        print(first_value / second_value)
+    except ZeroDivisionError:
+        print('Error: Zero division')
+elif operation == '//':
+    try:
+        print(first_value // second_value)
+    except ZeroDivisionError:
+        print('Error: Zero division')
 
 else:
     print(f'Invalid operation {operation}')
